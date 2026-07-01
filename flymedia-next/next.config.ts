3b004c601d34d11e65aa04f16e9c3376b662b24e
Paramjit singh <paramjeet.flymediatech@gmail.com>
2026-07-01 18:26:12 +0530

    add

--- flymedia-next/next.config.ts
+++ flymedia-next/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['sequelize'],
-  allowedDevOrigins: ['fly-pos.com'],
+  allowedDevOrigins: ['fly-pos.com', '*.fly-pos.com', 'localhost', '*.localhost'],
   
 };
 
