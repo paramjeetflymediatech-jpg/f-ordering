a0efddf1c736ac57ff52ecaa3828942fdb49ee8e
Paramjit singh <paramjeet.flymediatech@gmail.com>
2026-07-02 09:38:42 +0530

    remove indicatores

--- flymedia-next/next.config.ts
+++ flymedia-next/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['sequelize'],
   allowedDevOrigins: ['fly-pos.com', '*.fly-pos.com', 'localhost', '*.localhost'],
-  
+  devIndicators: false,
 };
 
 export default nextConfig;
